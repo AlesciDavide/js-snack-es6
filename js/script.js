951c1c500c9b017fab083af58619dc9fd7be0839
AlesciDavide <davi.alesci@email.com>
2024-05-23 14:33:07 +0200

    scrivo lista invitati

---
+++ js/script.js
@@ -0,0 +1,6 @@
+/* esercizio 1 */
+
+const nomiInvitati = [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello'];
+
+const segnaposto = [];
+
