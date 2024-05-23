c5d7c886d224f8c020f187edd92868bdf71716ed
AlesciDavide <davi.alesci@email.com>
2024-05-23 14:52:20 +0200

    sneck 2

--- js/script.js
+++ js/script.js
@@ -17,3 +17,66 @@ console.log(segnaposto);
 
 
 
+/* esercizio 2 */
+
+const elencoStudenti = [
+    {   
+        id: 213,
+        name: 'Giuseppina della rovere',
+        grades: 78,
+    },
+    {
+        id: 110,
+        name: 'Paola Cortellessa',
+        grades : 96,
+    },
+    {
+        id: 250,
+        name: 'Andrea Mantegna ',
+        grades : 48,
+    },
+    {
+        id: 145,
+        name: 'Gaia Borromini ',
+        grades : 74,
+    },
+    {
+        id: 196,
+        name: 'Luigi Grimaldello',
+        grades : 68,
+    },
+    {
+        id: 102,
+        name: 'Piero della Francesca',
+        grades : 50,
+    },
+    {
+        id: 100,
+        name: 'Francesca da Polenta',
+        grades : 84,
+    },
+]
+
+
+let nomeStudenti = [];
+let studentiSettanta = [];
+let studentiIdPlus = [];
+
+for (const key in elencoStudenti) {
+    let upperCase = elencoStudenti[key].name.toUpperCase();
+    nomeStudenti.push(upperCase);
+    
+    if(elencoStudenti[key].grades > 70){
+        studentiSettanta.push(elencoStudenti[key]);
+    };
+    if(elencoStudenti[key].id > 120 && elencoStudenti[key].grades > 70){
+        studentiIdPlus.push(elencoStudenti[key]);
+    }
+}
+
+console.log(nomeStudenti);
+console.log(studentiSettanta);
+console.log(studentiIdPlus);
+
+
+/* nomeStudenti.push(studente.name.toUpperCase()); */
